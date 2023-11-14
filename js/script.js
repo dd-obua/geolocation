218a3121bab1b38ec6796017c952869ffb12c7fd
Obua Denis Daniel <dd.obua@gmail.com>
2023-11-14 17:45:43 +0300

    Style input popup according to input type

--- js/script.js
+++ js/script.js
@@ -154,7 +154,7 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${type}-popup`,
         })
       )
       .setPopupContent('Workout')
