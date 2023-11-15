d5e063a929e013f9dbfcd0cdb000c421426d9c97
Obua Denis Daniel <dd.obua@gmail.com>
2023-11-15 10:44:39 +0300

    Add the submit event listener on the form

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ class App {
 
   constructor() {
     this._getPosition();
-    addEventListener('submit', this._createNewWorkout.bind(this));
+    form.addEventListener('submit', this._createNewWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
   }
 
